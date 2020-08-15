f71c8a128e3c22a40e9ed3310249e33dc5822683
sanghyeok-han <53252712+sanghyeok-han@users.noreply.github.com>
2020-08-15 14:30:27 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import { withStyles } from '@material-ui/core/styles'
 
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: 'auto'
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [{
   'id': 1,
@@ -31,25 +48,45 @@ const customers = [{
 
 class App extends Component {
   render() {
+    const { classes } = this.props
     return (
-      customers.map((customer) => {
-        return (
-          <Customer
-          key={customer.id}
-          id={customer.id}
-          image={customer.image}
-          name={customer.name}
-          birthday={customer.birthday}
-          gender={customer.gender}
-          job={customer.job}
-        />
-        )
-      }
-    ))
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {
+                customers.map((customer) => {
+                  return (
+                    <Customer
+                    key={customer.id}
+                    id={customer.id}
+                    image={customer.image}
+                    name={customer.name}
+                    birthday={customer.birthday}
+                    gender={customer.gender}
+                    job={customer.job}
+                  />
+                )})
+              }
+            </TableBody>
+          </Table>
+        </Paper>
+      </div>
+    )
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
 
 // C:\Users\User\Desktop\vsc 작업 공간\동빈나\React App\front>git remote add origin https://github.com/sanghyeok-han/React-Management-Tutorial.git
 
